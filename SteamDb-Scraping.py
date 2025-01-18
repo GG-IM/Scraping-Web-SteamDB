c21bb6165bd72a48fa9bb359a4c0a61d17169538
GG-IM <n00316076@upn.pe>
2025-01-18 18:42:54 -0500

    Add files via upload

---
+++ SteamDb-Scraping.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+
+driver=webdriver.Chrome()
+url="https://steamdb.info/charts/"
+driver.get(url)
+
+listanombre=[]
+listaactivos=[]
+lista24h=[]
+listapico=[]
+
+for num in range(1,40):
+    pagina=driver.find_element(By.XPATH,f'//button[contains(text(),{num})]')
+    pagina.click()
+    nombre_juego = driver.find_elements(By.XPATH, '//tr/td/a[normalize-space(text()) != ""]')
+    jugadores_activos=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][1]')
+    pico_24h=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][2]')
+    picohistorico=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][3]')
+
+    for nombre_completo, activos, pico24, picoH in zip(nombre_juego, jugadores_activos, pico_24h, picohistorico):
+        listanombre.append(nombre_completo.text)
+        listaactivos.append(activos.text)
+        lista24h.append(pico24.text)
+        listapico.append(picoH.text)
+
+datosunidos={'Nombre':listanombre,'Jugadores Activos':listaactivos,'Pico de 24H':lista24h ,'Pico Historico':listapico}
+df=pd.DataFrame(datosunidos,columns=['Nombre','Jugadores Activos','Pico de 24H','Pico Historico'])
+print(df)
+df.to_csv('datos_steam.csv',index=False)
