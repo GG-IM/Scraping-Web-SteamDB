173f5a7d97002a7819bd4acc36ec2617cb073ed7
GG-IM <n00316076@upn.pe>
2025-01-19 14:46:34 -0500

    Update SteamDb-Scraping.py

--- SteamDb-Scraping.py
+++ SteamDb-Scraping.py
@@ -12,19 +12,21 @@ listaactivos=[]
 lista24h=[]
 listapico=[]
 
-for num in range(1,40):
-    pagina=driver.find_element(By.XPATH,f'//button[contains(text(),{num})]')
-    pagina.click()
-    nombre_juego = driver.find_elements(By.XPATH, '//tr/td/a[normalize-space(text()) != ""]')
-    jugadores_activos=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][1]')
-    pico_24h=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][2]')
-    picohistorico=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][3]')
 
-    for nombre_completo, activos, pico24, picoH in zip(nombre_juego, jugadores_activos, pico_24h, picohistorico):
-        listanombre.append(nombre_completo.text)
-        listaactivos.append(activos.text)
-        lista24h.append(pico24.text)
-        listapico.append(picoH.text)
+pagina=driver.find_element(By.XPATH,f'//div[@class="dataTable_display"]/div[@class="dt-length"][1]/select[@class="dt-input"]')
+pagina.click()
+todolosdatos=driver.find_element(By.XPATH,f'//div[@class="dataTable_display"]/div[@class="dt-length"][1]/select[@class="dt-input"]/option[8]')
+todolosdatos.click()
+nombre_juego = driver.find_elements(By.XPATH, '//tr/td/a[normalize-space(text()) != ""]')
+jugadores_activos=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][1]')
+pico_24h=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][2]')
+picohistorico=driver.find_elements(By.XPATH,'//tr/td[@class="dt-type-numeric"][3]')
+
+for nombre_completo, activos, pico24, picoH in zip(nombre_juego, jugadores_activos, pico_24h, picohistorico):
+    listanombre.append(nombre_completo.text)
+    listaactivos.append(activos.text)
+    lista24h.append(pico24.text)
+    listapico.append(picoH.text)
 
 datosunidos={'Nombre':listanombre,'Jugadores Activos':listaactivos,'Pico de 24H':lista24h ,'Pico Historico':listapico}
 df=pd.DataFrame(datosunidos,columns=['Nombre','Jugadores Activos','Pico de 24H','Pico Historico'])
